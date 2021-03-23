4064c9de8804d51eb3271a23bcc124291e193f23
AnuragS01 <anuragsatyamsingh@gmail.com>
2021-03-23 12:28:56 +0530

    making some changes

--- src/components/card/card.components.jsx
+++ src/components/card/card.components.jsx
@@ -6,7 +6,7 @@ const Card = (props) => {
     <div className="card-container">
       <img
         alt="monsters"
-        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+        src={`https://robohash.org/1${props.monster.id}?set=set2&size=180x180`}
       />
       <h2>{props.monster.name}</h2>
       <p>{props.monster.email}</p>
